4b713504b1627bb5f7559b76dff6b389b891aa64
hicham <hicham@PC02.mshome.net>
2017-03-10 14:19:21 +0100

    subida de Hicham

---
+++ Ejer1/src/ejer1/Ejer1.java
@@ -0,0 +1,17 @@
+
+package ejer1;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.StringReader;
+
+/**
+ * @author hicham
+ */
+public class Ejer1 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("HOLA");
+    }
+    
+}
