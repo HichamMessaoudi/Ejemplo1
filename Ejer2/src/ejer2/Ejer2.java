f727ee7735722e50fff50b0cf29ddac6ba01caa6
HichamMessaoudi <hicham.madrid.1995.hm@gmail.com>
2017-03-15 14:35:55 +0100

    Update Ejer2.java
    ejemplo editado desde git

--- Ejer2/src/ejer2/Ejer2.java
+++ Ejer2/src/ejer2/Ejer2.java
@@ -8,5 +8,7 @@ public class Ejer2 {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("adios");
+        System.out.println("adios");
+        System.out.println("adios");
     }
 }
